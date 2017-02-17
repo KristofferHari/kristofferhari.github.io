cb9acc31ed80095d9bc2256574b44dde7bc0f6c9
JEstes93 <jestes93jobs@gmail.com>
2017-02-17 04:10:47 -0500

    Added email.js which ajax's formspree.io and sends emails to webmaster. Removed action attribute from contact form and added script reference to email.js in index.html

---
+++ js/email.js
@@ -0,0 +1,12 @@
+﻿//formspree.io ajax request
+// John Estes - https://github.com/JEstes93
+//02-17-2017
+
+$('#contactform').on('submit', function (e) {
+    $.ajax({
+        url: "//formspree.io/kristofferhari@gmail.com",
+        method: "post",
+        data: $(this).serialize(),
+        dataType: "json"
+    });
+});
